b45b58ab03430105e9cf1bb610e0df93477df0d4
John Conley <johnconley507@hotmail.com>
2019-12-22 16:49:37 -0500

    Creating new models and controller
    - Creating new models and controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Property;
+use App\Models\Rooms;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function show(Request $request) {
+        
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,31 +9,31 @@
           <li><span class="hotel_name" id="hotel_name">Sunset Key Guest Cottages&nbsp;<span class="vertical_line"></span><span class="stars" width="28" height="28">&nbsp;&nbsp;&#9733;&#9733;&#9733;&#9733;&#9733;</span></span></li>
           <li><span class="address" id="address">245 Front Street, Key West, USA</span></li>
           <li style="padding-top: 60px;">
-		  	<span class="room_dropdown" id="room_dropdown">
+		      <span class="room_dropdown" id="room_dropdown">
   		      <button onclick="showRoomDropdown()" class="room_info_btn" id="room_info_btn">
-    	  	  <div class="arrow_down" style="display: inline-table"></div> 
-    	  	    Rooms / Availability
-  		      </button>
-			</span>
-		  </li>
+    	 	    <div class="arrow_down" style="display: inline-table"></div> 
+    	       Rooms / Availability
+  	        </button>
+          </span>
+          </li>
   	    </ul>
       </div>
     </div>
     <div id="rm_drop" class="room_dropdown_content">
-	  <div class="row">
-	  <hr/>
+	    <div class="row">
+	      <hr/>
         <div class="column" id="room_type" style="text-transform:uppercase">Studio Suite City View</div>
         <div class="column2" id="availablity_type" style="text-transform:uppercase">On Request</div>
         <div class="column3"><div onclick="showRoomDetailsDropdown()" class="details_dropdown" id="details_dropdown" style="text-transform:uppercase">
-		  <span class="arrow_details" style="display: inline-table"></span> Details</div></div>
+		    <span class="arrow_details" style="display: inline-table"></span> Details</div></div>
         <div class="column4" id="price" style="text-transform:uppercase">593</div>
-		<span class="column5" id="usd" style="text-transform:capitalize">USD per night</span>
-		<button class="column6" id="request_btn" style="text-transform:uppercase">Request</button>
-		<div id="request_modal" class="modal"><div class="modal_content">
-		  <span class="close">&times;</span><p></p></div></div>
-	    <hr />
+  	   	<span class="column5" id="usd" style="text-transform:capitalize">USD per night</span>
+	    	<button class="column6" id="request_btn" style="text-transform:uppercase">Request</button>
+	  	  <div id="request_modal" class="modal"><div class="modal_content">
+	  	  <span class="close">&times;</span><p></p></div></div>
+	      <hr />
+	    </div>
 	  </div>
-	</div>
   </div>
   <div id="details_drop" class="details_dropdown_content">
     <div class="bed_row" id="bed_row">
@@ -41,43 +41,43 @@
       <button class="bedtype_daybed">Daybed</button>
     </div>
     <div class="date_row">
-	  <div class="current_date">Fri, Dec 9</div>
-	  <div class="horizontal_line"></div>
-	  <span class="date_row_price">593</span>	
-	  <span class="detail_usd"> USD</span>
-	</div>
-	<div class="condition_row">
-	  <div class="condition_column">
-	    <div class="condition_title">Conditions and Offers:</div>
-	    <div class="included"><span class="dot_one"></span> Meal Plan: Breakfast included</div>
-	    <div class="cancellation_title">Cancellation Policy:</div>
-	    <div class="cancellation_policy"><span class="dot_two"></span> Penalty of 1 booked night(s) when cancelling after Nov 25, 2016 04:00 PM</div>
+	    <div class="current_date">Fri, Dec 9</div>
+	    <div class="horizontal_line"></div>
+	    <span class="date_row_price">593</span>	
+	    <span class="detail_usd"> USD</span>
 	  </div>
+	  <div class="condition_row">
+	    <div class="condition_column">
+	      <div class="condition_title">Conditions and Offers:</div>
+	      <div class="included"><span class="dot_one"></span> Meal Plan: Breakfast included</div>
+	      <div class="cancellation_title">Cancellation Policy:</div>
+	      <div class="cancellation_policy"><span class="dot_two"></span> Penalty of 1 booked night(s) when cancelling after Nov 25, 2016 04:00 PM</div>
+	    </div>
+    </div>
 	  <div class="detail_column">
-	    <ul style="list-style-type:none;">
-		  <li><div class="price_detail">Price: </div></li>
-		  <li><div class="taxes_detail">Taxes 14%: </div></li>
-		  <li><div class="fee_detail">Fees: 0.00 p/nt</div></li>
-		  <li><div class="total_detail">Total: </div></li>
-		</ul>
-	  </div>
+     <ul style="list-style-type:none;">
+	      <li class="price_detail">Price: </li>
+	      <li class="taxes_detail">Taxes 14%: </li>
+    	  <li class="fee_detail">Fees: 0.00 p/nt</li>
+	   	  <li class="total_detail">Total: </li>
+	  	</ul>
+    </div>
 	  <div class="amounts_column">
 	    <ul style="list-style-type:none;">
-		  <li class="initial_amount">592.92</li>
-		  <li class="tax_amount">83.01</li>
-		  <li class="fee_pnt_amount">0.00 p/nt</li>
-		  <li class="total_amount">675.93</li>
-		</ul>
+	      <li class="initial_amount">592.92</li>
+	      <li class="tax_amount">83.01</li>
+	      <li class="fee_pnt_amount">0.00 p/nt</li>
+	      <li class="total_amount">675.93</li>
+	  	</ul>
 	  </div>
 	  <div class="usd_column">
 	    <ul style="list-style-type:none;">
-		  <li class="price_usd">USD</li>
-		  <li class="tax_usd">USD</li>
-		  <li class="fee_usd">USD</li>
-		  <li class="total_usd">USD</li>
-		</ul>
+	      <li class="price_usd">USD</li>
+	      <li class="tax_usd">USD</li>
+	      <li class="fee_usd">USD</li>
+	      <li class="total_usd">USD</li>
+	  	</ul>
 	  </div>
-	</div>
   </div>
 </body>
 
@@ -85,8 +85,8 @@
 
 // Opens Room Dropdown
   function showRoomDropdown() {
-	document.getElementById("rm_drop").classList.toggle("show");
-	document.getElementById("details_drop").classList.remove("show");
+	  document.getElementById("rm_drop").classList.toggle("show");
+	  document.getElementById("details_drop").classList.remove("show");
   };
 
 //   Closes Room Dropdown by clicking anywhere
@@ -142,17 +142,17 @@
   }
 
   .property_block {
-	background-color: #fff;
-	height: 247px;
-	width: 100%;
-	margin-bottom: 10px;
-	border: 2px solid #dadada;
+	  background-color: #fff;
+	  height: 247px;
+	  width: 100%;
+	  margin-bottom: 10px;
+	  border: 2px solid #dadada;
   }
 
   .picture_block {
-	margin-top: 30px;
-	margin-left: 30px;
-	font-family: avenir;
+	  margin-top: 30px;
+	  margin-left: 30px;
+	  font-family: avenir;
   }
 
   /* .hotel-info {
@@ -160,25 +160,25 @@
   } */
 
   .hotel_name {
-	color: #1EACDF;
-	font-size: 27px;
-	padding-left: 30px;
+	  color: #1EACDF;
+	  font-size: 27px;
+	  padding-left: 30px;
   }
 
   .vertical_line { 
     border-left: 2px solid #ababab; 
-	height: 35px; 
+  	height: 35px; 
     position:absolute; 
   }
 
   .address {
-	color: #ababab;
-	padding-left: 30px;
-	font-size: 21px;
+	  color: #ababab;
+	  padding-left: 30px;
+	  font-size: 21px;
   }
 
   .room_dropdown {
-	padding-left: 30px; 
+	  padding-left: 30px; 
   }
 
   .room_info_btn {
@@ -188,27 +188,27 @@
     padding: 11px 29px;
     font-size: 16px;
     cursor: pointer;
-	position:absolute;
+	  position:absolute;
   }
 
   .arrow_down {
     width: 0; 
     height: 0; 
-	padding-top: 3px;
+	  padding-top: 3px;
     border-left: 5px solid transparent;
     border-right: 5px solid transparent;
     border-top: 5px solid #fff;
   }
 
   .stars {
-	color: #FF4500; 
-	font-size: 20px; 
+	  color: #FF4500; 
+	  font-size: 20px; 
   }
 
  
   hr {
     width: 97%;
-	color: #a2a2a2;
+	  color: #a2a2a2;
   }
 
   .room_dropdown_content {
@@ -244,11 +244,11 @@
   }
 
   .show {
-	display: block;
+	  display: block;
   }
 
   .row {
-	padding-bottom: 30px;
+	  padding-bottom: 30px;
   }
 
   .column {
@@ -259,7 +259,7 @@
   .column2 {
     float: left;
     width: 10%;
-	padding-left: 140px;
+	  padding-left: 140px;
   }
 
   .column3 {
@@ -272,13 +272,13 @@
   .column4 {
     float: left;
     width: 3%;
-	padding-left: 5px;
+	  padding-left: 5px;
   }
 
   .column5 {
     width: 10%;
-	border: none;
-	margin-top:3px;
+	  border: none;
+	  margin-top:3px;
   }
 
   .column6 {
@@ -290,7 +290,7 @@
 
   .condition_column {
     width: 50%;
-	border: none;
+	  border: none;
   }
 
   .condition_row {
@@ -308,32 +308,32 @@
   .row:after {
     content: "";
     display: table;
-     clear: both;
+    clear: both;
   }
 
   #room_type {
-	margin-left: 19px;  
-	font-family: avenir;
-	color: #8a8a8a;
-	font-size: 18px;
-	font-weight: 500;
-	/* padding-top: 2px; */
+	  margin-left: 19px;  
+  	font-family: avenir;
+	  color: #8a8a8a;
+	  font-size: 18px;
+	  font-weight: 500;
+	  /* padding-top: 2px; */
   }
 
   #availablity_type {
-	font-family: avenir;
-	color: #FF8C00;
-	font-size: 17px;
-	/* padding-top: 2px; */
-	font-weight: bold;
+  	font-family: avenir;
+  	color: #FF8C00;
+  	font-size: 17px;
+  	/* padding-top: 2px; */
+  	font-weight: bold;
   }
 
   #details_dropdown {
-	font-family: avenir;
-	color: #1EACDF;
-	font-size: 17px;
-	/* padding-top: 2px; */
-	cursor: pointer;
+  	font-family: avenir;
+  	color: #1EACDF;
+  	font-size: 17px;
+	  /* padding-top: 2px; */
+	  cursor: pointer;
   }
 
   .detail_dropdown_content {
@@ -355,24 +355,24 @@
   .arrow_details {
     width: 0; 
     height: 0; 
-	padding-top: 4px;
+	  padding-top: 4px;
     border-left: 5px solid transparent;
     border-right: 5px solid transparent;
     border-top: 5px solid #1EACDF;
-	/* padding-top: 3px; */
-	cursor: pointer;
+	  /* padding-top: 3px; */
+	  cursor: pointer;
   }
 
   #price {
-	color: #000;
-	font-family: avenir;
-	font-size: 21px;
-	font-weight: bold;
-	margin-top: -3px;
+	  color: #000;
+	  font-family: avenir;
+	  font-size: 21px;
+	  font-weight: bold;
+	  margin-top: -3px;
   }
 
   #usd {
-	color: #ababab;
+	  color: #ababab;
     font-size: 17px;
     padding-right: 60px;
     font-family: avenir;
@@ -384,7 +384,7 @@
     /* padding-top: 2px; */
     font-weight: bold;
     margin-bottom: 3px;
-	cursor:pointer;
+	  cursor:pointer;
     font-family: avenir;
   }
   
@@ -451,7 +451,7 @@
   }
 
   .date_row {
-	padding-left: 50px;
+	  padding-left: 50px;
   }
 
   .current_date {
@@ -459,7 +459,7 @@
     font-family: avenir;
     padding-left: 26px;
     padding-top: 19px;
-	font-size: 15px;
+  	font-size: 15px;
   }
 
   .horizontal_line { 
@@ -469,22 +469,22 @@
   }
 
   .date_row_price {
-	color: #FF8C00;
+  	color: #FF8C00;
     padding-left: 25px;
     font-family: avenir;
-	font-weight: 600;
+  	font-weight: 600;
   }
 
   .detail_usd {
-	padding-left: 3px;
-	font-family: avenir;
-	color:#ababab;
+	  padding-left: 3px;
+  	font-family: avenir;
+  	color:#ababab;
   }
 
   .condition_title {
-	color: #8a8a8a;
-	padding-left: 19px;
-	font-family: avenir;
+  	color: #8a8a8a;
+  	padding-left: 19px;
+  	font-family: avenir;
     font-size: 16px;
     font-weight: 600;
   }
@@ -495,24 +495,24 @@
     background-color: #ababab;
     border-radius: 50%;
     display: inline-block;
-	margin-bottom: 2px;
-	font-family: avenir;
+  	margin-bottom: 2px;
+  	font-family: avenir;
   }
 
   .included {
-	padding-left: 8px;
-	font-family: avenir;
-	font-size: 16px;
-	color: #ababab;
+	  padding-left: 8px;
+  	font-family: avenir;
+  	font-size: 16px;
+  	color: #ababab;
   }
 
   .cancellation_title {
-	color: #8a8a8a;
-	padding-left: 19px;
-	font-family: avenir;
+  	color: #8a8a8a;
+  	padding-left: 19px;
+  	font-family: avenir;
     font-size: 16px;
     font-weight: 600;
-	padding-top: 13px;
+	  padding-top: 13px;
   }
 
   .dot_two {
@@ -521,20 +521,20 @@
     background-color: red;
     border-radius: 50%;
     display: inline-block;
-	margin-bottom: 2px;
-	font-family: avenir;
+  	margin-bottom: 2px;
+  	font-family: avenir;
   }
 
   .cancellation_policy {
-	padding-left: 8px;
-	font-family: avenir;
-	font-size: 16px;
-	color: red;
+  	padding-left: 8px;
+  	font-family: avenir;
+	  font-size: 16px;
+	  color: red;
     padding-bottom: 30px;
   }
 
   .pricing_row{
-	float: right;
+	  float: right;
   }
 
   .price_detail {
@@ -544,7 +544,7 @@
     padding-right: 170px;
     font-weight: 500;
     color: #ababab;
-	font-size: 17px;
+	  font-size: 17px;
   }
 
   .taxes_detail {
@@ -554,7 +554,7 @@
     padding-right: 170px;
     font-weight: 500;
     color: #ababab;
-	font-size: 17px;
+  	font-size: 17px;
   }
 
   .fee_detail {
@@ -564,7 +564,7 @@
     padding-right: 170px;
     font-weight: 500;
     color: #ababab;
-	font-size: 17px;
+	  font-size: 17px;
   }
 
   .total_detail {
@@ -574,7 +574,7 @@
     padding-right: 170px;
     font-weight: 600;
     color: #8a8a8a;
-	font-size: 17px;
+  	font-size: 17px;
   }
 
   .total_amount {
@@ -582,9 +582,9 @@
     float: right;
     margin-top: -72px;
     padding-right: 81px;
-	font-weight: 600;
+  	font-weight: 600;
     color: #8a8a8a;
-	font-size: 17px;
+  	font-size: 17px;
   }
 
   .fee_pnt_amount {
@@ -593,7 +593,7 @@
     margin-top: -97px;
     padding-right: 81px;
     color: #8a8a8a;
-	font-size: 17px;
+  	font-size: 17px;
   }
   
   .tax_amount {
@@ -602,7 +602,7 @@
     margin-top: -121px;
     padding-right: 81px;
     color: #8a8a8a;
-	font-size: 17px;
+  	font-size: 17px;
   }
 
   .initial_amount {
@@ -611,7 +611,7 @@
     margin-top: -146px;
     padding-right: 81px;
     color: #8a8a8a;
-	font-size: 17px;
+  	font-size: 17px;
   }
 
   .usd_column {
